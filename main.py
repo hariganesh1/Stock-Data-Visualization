c32645db5a2a3546095a916fe5ac8112429c78a7
Hari Ganesh <hganesh@umich.edu>
2024-06-05 15:21:56 -0700

    Deleted file i/o

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
 
 
-# ERROR: File not found issues. 
-# Deal with file I/O later
-'''file_name = input(
-    "Enter the filename of a yahoo finance .csv file with stock data.\n" 
-    "This file must be downloaded on your computer."
-) '''
 file = "AAPL (1)_full.csv"
 df = pd.read_csv(file)
 open = df['Open']
@@ -17,6 +10,8 @@ close = df['Close']
 vol = df['Volume']
 
 NUMROWS = df.shape[0]
+print(f"This stock has been available for trading for {NUMROWS} days.")
+
 
 ## Profit
 profit = []
@@ -27,39 +22,37 @@ print(f"The biggest absolute difference in the open price and the close price on
 
 ## Visualization: 
 fig = plt.figure()
+fig2 = plt.figure()
 # fig, axs = plt.subplots(2,2, sharey='row')
 
 
 
 # Open plot
-openPlot = fig.add_subplot(2,2,1)
+openPlot = fig.add_subplot(1,2,1)
 openPlot.plot(open)
 openPlot.set_title('Open Price Plot')
+openPlot.set_ylabel('Dollars')
 
 # Close plot
-closePlot = fig.add_subplot(2,2,2)
+closePlot = fig.add_subplot(1,2,2)
 closePlot.plot(close)
 closePlot.set_title('Close Price Plot')
 closePlot.set_ylabel('Dollars')
 
 
 # Profit plot
-profitPlot = fig.add_subplot(2,2,3)
+profitPlot = fig2.add_subplot(1,2,1)
 profitPlot.plot(profit)
 profitPlot.set_title('Profit Plot')
 profitPlot.set_ylabel('Dollars')
 
 # Volume plot
-volumePlot = fig.add_subplot(2,2,4)
+volumePlot = fig2.add_subplot(1,2,2)
 volumePlot.plot(vol)
 volumePlot.set_title('Stock Volume Plot')
 volumePlot.set_ylabel('Number of Shares (Millions)')
 
 fig.supxlabel('Days since stock opened')
+fig2.supxlabel('Days since stock opened')
 
 plt.show()
-
-
-
-
-
